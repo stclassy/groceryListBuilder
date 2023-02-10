5e10cbd4a93d08e2870b779a754dc14f9dc9c294
stclassy <stclsn02@gmail.com>
2023-02-10 16:38:24 -0600

    added User entity, user dao, session factory provider, and hibernate config files, also updated log4j properties and pom xml

---
+++ src/main/java/com/groceryListBuilder/entity/User.java
@@ -0,0 +1,179 @@
+package com.groceryListBuilder.entity;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * A class to represent a User.
+ *
+ * @author Sydney St. Clair
+ */
+@Entity(name = "User")
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int userId;
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+
+    /**
+     * Instantiates a new User.
+     */
+    public User () {
+    }
+
+    /**
+     * Instantiates a new User.
+     *
+     * @param userId     the user id
+     * @param firstName  the first name
+     * @param lastName   the last name
+     * @param userName   the user name
+     * @param password   the password
+     * @param createDate the create date
+     */
+    public User(int userId, String firstName, String lastName, String userName, String password, LocalDateTime createDate) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.createDate = createDate;
+    }
+
+    /**
+     * Gets user id.
+     *
+     * @return the user id
+     */
+    public int getUserId() {
+        return userId;
+    }
+
+    /**
+     * Sets user id.
+     *
+     * @param userId the user id
+     */
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * Gets first name.
+     *
+     * @return the first name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets first name.
+     *
+     * @param firstName the first name
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Gets last name.
+     *
+     * @return the last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets last name.
+     *
+     * @param lastName the last name
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Gets user name.
+     *
+     * @return the user name
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Sets user name.
+     *
+     * @param userName the user name
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * Gets password.
+     *
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Sets password.
+     *
+     * @param password the password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Gets create date.
+     *
+     * @return the create date
+     */
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    /**
+     * Sets create date.
+     *
+     * @param createDate the create date
+     */
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    /**
+     * Gets update date.
+     *
+     * @return the update date
+     */
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    /**
+     * Sets update date.
+     *
+     * @param updateDate the update date
+     */
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+}
